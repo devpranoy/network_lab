77ae07568708d83889a49f0e4cc5481e41c94fbe
Pranoy Dev <devprnoy@gmail.com>
2018-02-20 18:02:41 +0530

    File transfer over socket

---
+++ lab7/client.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.net.*;
+
+public class client{
+	
+	private Socket s;
+	
+	public client(String host, int port, String file) {
+		try {
+			s = new Socket(host, port);
+			sendFile(file);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}		
+	}
+	
+	public void sendFile(String file) throws IOException {
+		DataOutputStream dos = new DataOutputStream(s.getOutputStream());
+		FileInputStream fis = new FileInputStream(file);
+		byte[] buffer = new byte[4096];
+		
+		while (fis.read(buffer) > 0) {
+			dos.write(buffer);
+		}
+		
+		fis.close();
+		dos.close();	
+	}
+	
+	public static void main(String[] args) {
+		client fc = new client("localhost", 8001, "1.txt");
+	}
+
+}
---
+++ lab7/server.java
@@ -0,0 +1,52 @@
+import java.io.*;
+import java.net.*;
+
+public class server extends Thread {
+	
+	private ServerSocket ss;
+	
+	public server(int port) {
+		try {
+			ss = new ServerSocket(port);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void run() {
+		while (true) {
+			try {
+				Socket clientSock = ss.accept();
+				saveFile(clientSock);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	private void saveFile(Socket clientSock) throws IOException {
+		DataInputStream dis = new DataInputStream(clientSock.getInputStream());
+		FileOutputStream fos = new FileOutputStream("2.txt");
+		byte[] buffer = new byte[1024];
+		
+		int filesize = 50; // Send file size in separate msg
+		int read = 0;
+		int totalRead = 0;
+		int remaining = filesize;
+		while((read = dis.read(buffer, 0, Math.min(buffer.length, remaining))) > 0) {
+			totalRead += read;
+			remaining -= read;
+			System.out.println("read " + totalRead + " bytes.");
+			fos.write(buffer, 0, read);
+		}
+		
+		fos.close();
+		dis.close();
+	}
+	
+	public static void main(String[] args) {
+		server fs = new server(8001);
+		fs.start();
+	}
+
+}
